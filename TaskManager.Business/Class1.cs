6d0f5e63366c2394b4993e18db1e5c3cb9d00139
Iago Ferreira <iago-ferreira@outlook.com>
2024-01-19 13:00:28 -0300

    chore: added base project structure

---
+++ TaskManager.Business/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManager.Business;
+
+public class Class1
+{
+
+}
---
+++ TaskManager.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TaskManager.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
